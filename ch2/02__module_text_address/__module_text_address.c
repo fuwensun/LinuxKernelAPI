f09c95bba5ff290416f0f21017da8c0a3e78bac8
sfw <u18a@sfw.com>
2018-07-03 03:38:13 +0800

    ch2 __module_text_address

---
+++ ch2/02__module_text_address/__module_text_address.c
@@ -0,0 +1,60 @@
+/*
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init __module_text_address_init(void);
+static void __exit __module_text_address_exit(void);
+
+int fun_a(void){
+    return 0;
+}
+
+static int var_b = 4;
+
+int __init __module_text_address_init(void){
+
+    unsigned long addr = 0;     
+    struct module *ret = 0;
+
+    addr = (unsigned long)fun_a;
+    preempt_disable();
+    ret = __module_text_address(addr);
+    preempt_enable();
+
+    printk("it's about fun_a:\n");
+    if (ret != NULL){
+        printk("ret->name: %s\n",ret->name);
+        printk("ret->state: %d\n",ret->state);
+        printk("ret->percpu_size: %d\n",ret->percpu_size);
+    }else{
+        printk("fun_a is not in text area!\n"); 
+    }
+
+    addr = (unsigned long)var_b;
+    preempt_disable();
+    ret = __module_text_address(addr);
+    preempt_enable();
+
+    printk("it's about var_b:\n");
+    if (ret != NULL){
+        printk("ret->name: %s\n",ret->name);
+        printk("ret->state: %d\n",ret->state);
+        printk("ret->percpu_size: %d\n",ret->percpu_size);
+    }else{
+        printk("var_b is not in text area!\n"); 
+    }
+    return 0;
+}
+
+void __exit __module_text_address_exit(void){
+    printk(KERN_EMERG"module exit ok!\n");
+}
+
+module_init(__module_text_address_init);
+module_exit(__module_text_address_exit);
+
