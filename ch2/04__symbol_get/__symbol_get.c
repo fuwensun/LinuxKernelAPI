535d6167b142bf9cf5d012eeaa1b1d4427222249
sfw <u18a@sfw.com>
2018-07-04 01:54:59 +0800

    ch2-04__symbol_get

---
+++ ch2/04__symbol_get/__symbol_get.c
@@ -0,0 +1,45 @@
+/*
+ *linux/kernel/module.c
+ *void __symbol_get(const char *symbol)
+ *如果符号存在，返回地址，引用加一；否则返回NULL
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init __symbol_get_init(void);
+static void __exit __symbol_get_exit(void);
+
+int __init __symbol_get_init(void){
+    const char *symbol_name = 0;
+    void *addr = 0;
+
+    symbol_name = "cpu_info";
+    addr = __symbol_get(symbol_name);
+
+    if (addr != NULL){
+        printk("the address of %s is: %lx\n",symbol_name,(unsigned long)addr);
+    }else{
+        printk("%s isn't found\n",symbol_name);
+    }
+
+    symbol_name = "symbol_0";
+    addr = __symbol_get(symbol_name);
+
+    if (addr != NULL){
+        printk("the address of %s is: %lx\n",symbol_name,(unsigned long)addr);
+    }else{
+        printk("%s isn't found\n",symbol_name);
+    }
+
+    return 0;
+}
+
+void __exit __symbol_get_exit(void){
+    printk("module <%s> exit ok!\n",THIS_MODULE->name);
+}
+
+module_init(__symbol_get_init);
+module_exit(__symbol_get_exit);
+
+MODULE_LICENSE("GPL");
