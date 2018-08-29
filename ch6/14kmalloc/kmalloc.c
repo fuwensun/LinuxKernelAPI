8edb388dcb67449a8a2a5986a63ceb5fa6e4dc6c
sfw <sfuwen@sina.com>
2018-08-29 20:04:15 +0800

    add ch6/14

---
+++ ch6/14kmalloc/kmalloc.c
@@ -0,0 +1,40 @@
+/*
+linux/slab.h
+static __always_inline void *kmalloc(size_t size, gfp_t flags)
+以flags的方式分配size虚拟内存，成功返回虚拟地址；失败返回NULL。
+*/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#define MEM_ALLOC_SIZE 8192
+char *mem_spvm = NULL;
+
+static int __init kmalloc_init(void){
+	
+	mem_spvm = (char *)kmalloc(MEM_ALLOC_SIZE,GFP_KERNEL);
+	if(!mem_spvm){
+		printk("kcalloc failed!\n");
+	}else{
+		printk("kcalloc successfullly! addr = 0x%lx\n",(unsigned long)mem_spvm);
+		printk("the actual allocatied size is: %d\n",(unsigned int)ksize(mem_spvm));//sfw** ksize()
+		printk("the content of mem_spvm+10 is: %d\n",*(mem_spvm+10));
+		printk("the content of mem_spvm+1000 is: %d\n",*(mem_spvm+1000));
+	}
+
+	return 0;
+}
+
+static void __exit kmalloc_exit(void){
+	if(!mem_spvm){
+		kfree(mem_spvm);
+		printk("kfree ok!\n");
+	}
+	printk("exit!");
+}
+
+module_init(kmalloc_init);
+module_exit(kmalloc_exit);
+
+MODULE_LICENSE("GPL");
