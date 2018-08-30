6a19cd301da0909ae0f6e728e7123edcf621e20c
sfw <sfuwen@sina.com>
2018-08-30 11:27:27 +0800

    add ch6/18

---
+++ ch6/18kmem_cache_zalloc/kmem_cache_zalloc.c
@@ -0,0 +1,48 @@
+/*
+linux/slab.h
+void *kmem_cache_zalloc(struct kmem_cache *cachep, gfp_t flags)
+以flags的方式从给定缓存cachep分配一个对象并置零。
+*/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+struct kmem_cache *my_cachep = NULL;
+void *object = NULL;
+
+static int __init kmem_cache_zalloc_init(void){
+	my_cachep = kmem_cache_create("my_cache",35,0,SLAB_HWCACHE_ALIGN,NULL);
+	if(!my_cachep){
+		printk("kmem_cache_create failed!\n");
+	}else{
+		object = kmem_cache_zalloc(my_cachep,GFP_KERNEL);
+		if(!object){
+			printk("kmem_cache_zalloc failed!\n");
+		}else{
+			printk("allocate object is 0x%lx\n",(unsigned long)object);
+			printk("the content of object+2 is: %d\n",*((char *)object+2));
+			printk("the content of object+20 is: %d\n",*((char *)object+20));
+		}
+	}
+	return 0;
+}
+
+static void __exit kmem_cache_zalloc_exit(void){
+	if(object){
+		kmem_cache_free(my_cachep,object);
+		printk("free object successfully!\n");
+	}
+
+	if(my_cachep){
+		kmem_cache_destroy(my_cachep);
+		printk("destroy my_cachep successfully!");
+	}
+	printk("exit!\n");
+	return;
+}
+
+module_init(kmem_cache_zalloc_init);
+module_exit(kmem_cache_zalloc_exit);
+
+MODULE_LICENSE("GPL");
