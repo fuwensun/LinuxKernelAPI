9ebdf5a38f091964a8de72b93ef04aad3336dde7
sfw <sfuwen@sina.com>
2018-09-06 09:26:58 +0800

    fix ch2/01

--- ch2/01__module_address/__module_address.c
+++ ch2/01__module_address/__module_address.c
@@ -3,6 +3,7 @@
  * struct module *__module_address(unsigned long add)
  * 根据内存地址add，获得地址所在的module
  */
+
 #include<linux/module.h>
 #include<linux/init.h>
 
@@ -15,26 +16,24 @@ int a_module(void){
 	return 0;
 }
 
-struct module *ret;
+struct module *ret = NULL;
+
 int __init __module_address_init(void){
 
-//    struct module *ret;
     unsigned long addr = (unsigned long)a_module;
 
     preempt_disable();
     ret = __module_address(addr);
     preempt_enable();
 
-    if (ret != NULL){
-
-        printk(KERN_EMERG"ret->name: %s\n",ret->name);
-        printk(KERN_EMERG"ret->state: %d\n",ret->state);
-        printk(KERN_EMERG"ret->percpu_size: %d\n",ret->percpu_size);
-        printk(KERN_EMERG"refs of %s is %d \n",ret->name,module_refcount(ret));
-    }else{
-
+    if (!ret){
         printk(KERN_EMERG"__module_address return NULL!\n");
+		return -ENOMEM;
     }
+
+	printk(KERN_EMERG"ret->name: %s\n",ret->name);
+    printk(KERN_EMERG"ret->state: %d\n",ret->state);
+    printk(KERN_EMERG"ret->percpu_size: %d\n",ret->percpu_size);
 
     return 0;
 }
@@ -45,6 +44,7 @@ void __exit __module_address_exit(void){
     printk(KERN_EMERG"ret->state: %d\n",ret->state);
     printk(KERN_EMERG"module exit ok!\n");
     printk(KERN_EMERG"ret->state: %d\n",ret->state);
+	return;
 }
 
 module_init(__module_address_init);
